ba15bfb1d6c3f355350805a5c271345bf1c696da
adhammeligy <adham5846@aucegypt.edu>
2022-11-02 17:43:04 +0200

    adham

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
 use std::{net::UdpSocket, io};
+use std::time;
+use std::thread;
 fn main() {
     let socket  = UdpSocket::bind("127.0.0.1:7879").expect("couldn't bind to address");
+    let sock = UdpSocket::bind("127.0.0.1:21544").expect("couldnt bind to address");
+    
+    let thread_join_handle = thread::spawn(move || {
+        send_request(&socket);
+    });
+
+    let thread_join_handle2 = thread::spawn(move || {
+        agent(&sock);
+    });
+
+    let _res = thread_join_handle.join();
+    thread_join_handle2.join().unwrap();
+
+
+}
+
+fn send_request(socket : &UdpSocket){
     loop {
+        let duration = time::Duration::from_secs(1);
+        socket.set_read_timeout(Some(duration)).unwrap();
         let mut buf = [0;1000];
         let mut request = String::new();
         io::stdin()
@@ -18,9 +39,11 @@ fn main() {
         }
         } 
         
-    // let (_, src_addr) = socket.recv_from(&mut buf).expect("Didn't receive data");
-    // println!("Recieved successsfully from {}",src_addr);
-    // let reply = String::from("Ack");
-    // let reply =reply.as_bytes();
-    // socket.send_to(reply, src_addr).expect("couldn't send data");
+}
+
+fn agent(socket : &UdpSocket){
+    let duration = time::Duration::from_secs(1);
+    socket.set_read_timeout(Some(duration)).unwrap();
+    let mut buf = [0;1000];
+    
 }
